b418e0f87ddf6cdab25811e1a6962680389f8fa9
jeanettelandoy <32128682+jeanettelandoy@users.noreply.github.com>
2020-03-08 20:20:10 +0100

    console log api

--- project_vue/src/API/APIClient.js
+++ project_vue/src/API/APIClient.js
@@ -1,5 +1,5 @@
 
-import axios from 'axios'
+
 
 
 /*eslint-disable*/
@@ -10,90 +10,40 @@ const USER_KEY = '59aeb1b217ba38dc388929d3f9aa29da'
 
 export default class APIClient {
 
-  // Get the 30s most popular games from IGDB + V2
-  /* static getGames () {
-    return new Promise((resolve, reject) => {
-      axios.get(`${API_URL}/games/?fields=name,cover,summary,url&popularity&order=popularity:desc&limit=30&category=0`, {
-        headers: {
-          "user-key": USER_KEY,
-          Accept: "application/json"
-        }
-      })
-        .then(response => {
-          console.log(response.data)
-          resolve(response.data)
-        })
-        .catch(e => {
-          reject(e)
-        })
-    })
-  } */
-
   // Get the 30s most popular games from IGDB + V3
-  static getGames () {
-    return new Promise((resolve, reject) => {
-      axios({
-        url: API_URL_V3,
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'user-key': USER_KEY
-        },
-        data: "fields name,cover,summary,url;sort popularity desc;limit 30;"
-      })
-        .then(response => {
-          resolve(response.data);
-        })
-        .catch(err => {
-          reject(err);
-        });
-    });
-  }
+   static getGames () {
+  //   return new Promise((resolve, reject) => {
+  //     axios({
+  //       url: API_URL_V3,
+  //       method: 'POST',
+  //       headers: {
+  //         'Accept': 'application/json',
+  //         'user-key': USER_KEY
+  //       },
+  //       data: "fields name,cover,summary,url;sort popularity desc;limit 30;"
+  //     })
+  //       .then(response => {
+  //         resolve(response.data);
+  //       })
+  //       .catch(err => {
+  //         reject(err);
+  //       });
+  //   });
+  
 
-  // Get the 30s most popular games with a specific given name + V3
-  static searchGames (gameName) {
-    return new Promise((resolve, reject) => {
-      axios({
-        url: API_URL_V3,
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'user-key': USER_KEY
-        },
-        data: "search " + gameName + ";fields name,cover,summary,url;sort popularity desc;limit 30;"
-      })
-        .then(response => {
-          resolve(response.data);
-        })
-        .catch(err => {
-          reject(err);
-        });
-    });
-  }
-
-  // Supposed to handle CORS with V3 + Doesn't work
-  static registerHeader () {
-    return new Promise((resolve, reject) => {
-      axios({
-        url: API_HEADER,
-        method: 'GET',
-        headers: {
-          'Accept': 'application/json',
-          'user-key': USER_KEY
-        },
-        data: {
-          "api_header": {
-            "header": "Access-Control-Allow-Origin",
-            "value": "*"
-          }
-        }
-      })
-        .then(response => {
-          console.log(response.data);
-        })
-        .catch(err => {
-          console.error(err);
-        });
-    });
-  }
+  var myHeaders = new Headers();
+  myHeaders.append("user-key", "59aeb1b217ba38dc388929d3f9aa29da");
+  
+  var requestOptions = {
+    method: 'GET',
+    headers: myHeaders,
+    redirect: 'follow'
+  };
+  
+  fetch("https://api-v3.igdb.com/games/", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
 }
+}
+
